3162dbe22cb13500b011b23ee321036e2abf03ee
p33t <p33t@localhost>
2025-08-22 12:43:21 -0700

    Add full-text-search fields (V1)

---
+++ app/Migrations/20250821234100_AddFts.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace app.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<NpgsqlTsVector>(
+                name: "VectorEn",
+                table: "HotelReview",
+                type: "tsvector",
+                nullable: false)
+                .Annotation("Npgsql:TsVectorConfig", "english")
+                .Annotation("Npgsql:TsVectorProperties", new[] { "Title", "Text" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelReview_VectorEn",
+                table: "HotelReview",
+                column: "VectorEn")
+                .Annotation("Npgsql:IndexMethod", "GIN");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HotelReview_VectorEn",
+                table: "HotelReview");
+
+            migrationBuilder.DropColumn(
+                name: "VectorEn",
+                table: "HotelReview");
+        }
+    }
+}
