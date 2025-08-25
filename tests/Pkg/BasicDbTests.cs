89c225d39f85dfa7c4cff84cb991163bbc69dcf6
p33t <p33t@localhost>
2025-08-25 14:45:03 -0700

    Illustrate problem with editing lean objects

--- tests/Pkg/BasicDbTests.cs
+++ tests/Pkg/BasicDbTests.cs
@@ -38,7 +38,7 @@ public class BasicDbTests(TestFixture fixture, ITestOutputHelper outputHelper) :
     public async Task Select_AvoidsLoadingVectorFields()
     {
         // fixture.SetupLogging(outputHelper);
-        
+
         await fixture.WithScopeAsync(async sp =>
         {
             var db = sp.GetRequiredService<AppDb>();
@@ -49,9 +49,63 @@ public class BasicDbTests(TestFixture fixture, ITestOutputHelper outputHelper) :
                 // .Select(HotelReview.LEAN)
                 // .ToListAsync();
                 .ToListLeanAsync();
-            
+
             Assert.NotEmpty(results);
             Assert.All(results, x => Assert.Null(x.VectorEn));
+        });
+    }
+
+    [Fact]
+    public async Task UpdateMappedEntry_DoesNotWork()
+    {
+        // fixture.SetupLogging(outputHelper);
+
+        await fixture.WithScopeAsync(async sp =>
+        {
+            var db = sp.GetRequiredService<AppDb>();
+
+            var review0 = await db.HotelReview.AsTracking()
+                .OrderBy(x => x.Id)
+                .Select(HotelReview.LEAN)
+                .FirstAsync();
+
+            review0.Text = $"random value {Random.Shared.Next()}";
+            var count = await db.SaveChangesAsync();
+            Assert.Equal(0, count); // >>>> isn't saved
+        });
+    }
+
+    [Fact]
+    public async Task UpdateMappedEntry_WorksIfManuallyAttached()
+    {
+        // fixture.SetupLogging(outputHelper);
+
+        await fixture.WithScopeAsync(async sp =>
+        {
+            var db = sp.GetRequiredService<AppDb>();
+
+            var review0 = await db.HotelReview
+                .OrderBy(x => x.Id)
+                .Select(HotelReview.LEAN)
+                .FirstAsync();
+
+            db.HotelReview.Attach(review0);
+            var origText = review0.Text;
+            var uniqueString = "thisuniquestringshouldonlyoccuronce";
+            review0.Text = uniqueString;
+            var count = await db.SaveChangesAsync();
+            Assert.Equal(1, count);
+            db.Entry(review0).State = EntityState.Detached;
+
+            var altReview0 = await db.HotelReview.AsTracking()
+                .Where(x => x.VectorEn.Matches(EF.Functions.PlainToTsQuery("english", uniqueString)))
+                .Select(HotelReview.LEAN)
+                .SingleAsync();
+
+            db.HotelReview.Attach(altReview0);
+            altReview0.Text = origText;
+            var altCount = await db.SaveChangesAsync();
+            Assert.Equal(1, altCount);
         });
     }
 }
